181ae3882259167a3e96e9e67998070288efd76e
Guy McKechnie <mckechnieguy.mg@gmail.com>
2025-02-12 22:55:11 +0200

    `Removed DatabaseController.java and updated Main.java to reflect changes in database operations and user management.`

--- src/main/java/com/booklidio/Main.java
+++ src/main/java/com/booklidio/Main.java
@@ -1,9 +1,16 @@
 package com.booklidio;
 
-import com.booklidio.Database.DatabaseController;
+import com.booklidio.DatabaseDir.DatabaseController;
 
 public class Main {
     public static void main(String[] args) {
-        DatabaseController.addUser("Guy", "McKechnie", "39 Johnson Street", "Deneysville");
+        DatabaseController.addUser("Guy", "McKechnie", "guy@gmail.com", "Password123", "012 345 6789", 0);
+        DatabaseController.addUser("Amy", "McKechnie", "amy@gmail.com", "Password567", "987 654 3210", 1);
+        System.out.println(DatabaseController.getUser("amy@gmail.com", "Password567"));
+        System.out.println(DatabaseController.getUser("guy@gmail.com", "Password123"));
+        // DatabaseController.removeUser(DatabaseController.getUser("amy@gmail.com",
+        // "Password567"));
+        // DatabaseController.removeUser(DatabaseController.getUser("guy@gmail.com",
+        // "Password123"));
     }
 }
---
+++ src/main/java/com/booklidio/UserDir/Seller.java
@@ -0,0 +1,5 @@
+package com.booklidio.UserDir;
+
+public class Seller {
+    
+}
